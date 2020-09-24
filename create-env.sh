ac84912188ae5df7a14157b42682761c60d6b3ea
Joseph Fulkerson <jkfulkerson@hawk.iit.edu>
2020-09-23 22:09:11 -0500

    Query Statements Added

--- create-env.sh
+++ create-env.sh
@@ -8,38 +8,35 @@
 
 
 aws ec2 run-instances \
---image-id ami-06b263d6ceff0b3dd \
---instance-type t2.micro \
---security-groups sg-0578335deaea2113a8 \
---key-name desktoppc-ubuntu-vm2.priv \
---count 3 \
---user-data file://install-env.sh ##Script executes upon launching of instance.
+    --image-id ami-0817d428a6fb68645 \
+    --instance-type t2.micro \
+    --security-group-ids sg-08546f3802f02691 \
+    --key-name desktoppc-ubuntu-vm2.priv \
+    --availability-zones us-east-1a \
+    --count 3 \
+    --user-data file://install-env.sh ##Script executes upon launching of instance.
 
 
 
 aws ec2 wait instance-running \
-    --instance-ids $(aws ec2 describe-instances --query 'Reservations[*].Instances[*].{Instance:InstanceId}' ) \
+    --instance-ids $(aws ec2 describe-instances --query 'Reservations[].Instances[*].{Instance:InstanceId}' --output text ) \
 
 
 
 aws elbv2 create-load-balancer \
     --name jf-ITMO-balance \
     --subnets subnet-1779d571 subnet-4ac5a344 \
-
-
-aws elbv2 describe-load-balancer-arns  \
-    --output  = `load_balance_arn[*]` \
-    --query Reservation[0].{`arn`} \
+    --security-group-ids sg-08546f3802f02691 \
 
 
 aws elbv2 wait load-balancer-exists \
-    --load-balancer-arns $(load_balance_arn[0],load_balance_arn[1],load_balance_arn[2],) \
+    --load-balancer-arns $(aws elbv2 describe-load-balancers --query 'LoadBalancers[].LoadBalancerArn[*].{LoadBalancerArn}' --output text) \
     --names jf-ITMO-balance \
 
 
 
 aws ec2 describe load-balancer-available \
-    --load-balancer-arns $(load_balance_arn) \
+    --load-balancer-arns $(aws elbv2 describe-load-balancers --query 'LoadBalancers[].LoadBalancerArn[*].{LoadBalancerArn}' --output text) \
     --names jf-ITMO-balance \
 
 
@@ -51,27 +48,22 @@ aws elbv2 create-target-group \
     --target-type instance \
     --vpc-id 71c1390c
 
-
-aws elbv2 describe-target-groups \
-    --output = `target_group_arn` \
-    --query Reservations[0].{`arn`} \
-
 aws elbv2 register-targets \
-    --target-group-arn $(target_group_arn) \
-    --targets Id=$(instance_id) \
+    --target-group-arn $(aws elbv2 describe-target-groups --query 'TargetGroups[*].TargetGroupArn[*].{TargetGroupArn}' --output text) \
+    --targets Id=$(aws ec2 describe-instances --query 'Reservations[].Instances[*].{Instance:InstanceId}' --output text ) \
 
 
 
 aws elbv2 create-listener \
 
-    --load-balancer-arn $(load_balancer_arn) \
+    --load-balancer-arn $(aws elbv2 describe-load-balancers --query 'LoadBalancers[].LoadBalancerArn[*].{LoadBalancerArn}' --output text) \
     --protocol HTTP \
     --port 80 \
-    --default-actions Type=forward,TargetGroupArn=arn:aws:elasticloadbalancing:us-west-2:123456789012:targetgroup/my-targets/73e2d6bc24d8a067 \
+    --default-actions Type=forward,TargetGroupArn=$(aws elbv2 describe-load-balancers --query 'LoadBalancers[].LoadBalancerArn[*].{LoadBalancerArn}' --output text) \
 
 #Arn of target group
 aws elbv2 modify-target-group \
-    --target-group-arn $(target_group_arn) \
+    --target-group-arn $(aws elbv2 describe-load-balancers --query 'LoadBalancers[].LoadBalancerArn[*].{LoadBalancerArn}' --output text) \
     --health-check-protocol HTTP \
     --health-check-port 80 \
 
